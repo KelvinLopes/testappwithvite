6b0106dcdf843fa2164e64146a9085ef7fcf7ca7
KelvinLopes <kelvin7android@gmail.com>
2022-05-02 02:16:25 +0000

    Routes

---
+++ testingwithdevite/src/Routes.tsx
@@ -0,0 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import { Cart } from "./pages/Cart";
+import { Catalog } from "./pages/Catalog";
+import { Home } from "./pages/Home";
+
+export function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
+    </Router>
+  )
+}
